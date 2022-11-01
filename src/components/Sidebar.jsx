7495ce3917f77f73b763eb506473d0ef388d1fea
sidneimv <sidneimv@gmail.com>
2022-11-01 14:12:09 -0300

    commit #8

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,7 +12,7 @@ const Sidebar = () => {
       <p className="title">Desenvolvedor</p>
       <SocialNetworks />
       <InformationContainer />
-      <a href="/src/components/CSS_Selector.pdf" download="CV.pdf" target="_blank" className="btn">
+      <a href="/src/components/CSS_Selector.pdf" download target="_blank" className="btn">
         Download CV 
       </a>
     </aside>
