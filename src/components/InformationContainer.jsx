624c8f73ed468cbb74843ca850b8f32e7bc858a0
sidneimv <sidneimv@gmail.com>
2022-11-02 15:45:03 -0300

    commit 2.0

--- src/components/InformationContainer.jsx
+++ src/components/InformationContainer.jsx
@@ -1,5 +1,7 @@
 import {AiFillPhone, AiOutlineMail, AiFillEnvironment} from "react-icons/ai"
 
+import WhatsApp from "../img/whatsapp.png"
+
 import "../styles/components/informationcontainer.sass"
 
 const InformationContainer = () => {
@@ -9,14 +11,16 @@ const InformationContainer = () => {
         <AiFillPhone id="phone-icon" />
         <div>
           <h3>Telefone</h3>
-          <p>(11)95772-8485</p>
+          <a href="https://api.whatsapp.com/send?phone=5511957728485" target="_blank" title="Abrir no WhatsApp">
+            (11)95772-8485
+          </a>
         </div>
       </div>
       <div className="info-card">
         <AiOutlineMail id="email-icon" />
         <div>
           <h3>e-Mail</h3>
-          <p>sidneimv@gmail.com</p>
+          <a href="mailto:sidneimv@gmail.com">sidneimv@gmail</a>
         </div>
       </div>
       <div className="info-card">
